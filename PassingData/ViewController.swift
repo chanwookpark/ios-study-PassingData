04b964ac4ca4296e5d218d54652b2500fead2743
chanwook <chanwook.god@gmail.com>
2016-03-21 08:01:14 +0900

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by chanwook on 2016. 3. 21..
+//  Copyright © 2016년 chanwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
